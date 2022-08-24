4eca710e2efcc0bf65718554828a304da98a8a1a
ZanikaH <112009982+ZanikaH@users.noreply.github.com>
2022-08-24 13:36:09 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/***************************************************************************Hello.java
+****************************************************************************Zanika Hossain
+*8/24/2022
+*CMSC255 004
+* 
+****************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Zanika Hossain");
+		System.out.println("Hello.java");
+		System.out.println("Writing my first programing for this class!");
+		System.out.println("8/24/2022");
+		System.out.println("CMSC255 004");
+		System.out.println();
+	}
+}
